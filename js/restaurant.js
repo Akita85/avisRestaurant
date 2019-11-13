b167808c691f08878a50f29b06f80546d2078398
Audrey <audreykfrancois@gmail.com>
2019-11-13 22:11:51 +0100

    mise en place des informations restaurant avec notation et avis

--- js/restaurant.js
+++ js/restaurant.js
@@ -1,27 +1,23 @@
-var restaurantID = document.getElementById('restaurants');
+const restaurantID = document.getElementById('restaurants');
 //var restaurantDetails = document.getElementById('RestaurantDetails');
 
-var requestULR = 'https://raw.githubusercontent.com/Akita85/avisRestaurant/master/json/restaurant.json';
-var request = new XMLHttpRequest();
+const requestULR = 'https://raw.githubusercontent.com/Akita85/avisRestaurant/master/json/restaurant.json';
+let request = new XMLHttpRequest();
 request.open('GET', requestULR);
 request.responseType = 'json';
 request.send();
 
 request.onload = function() 
 {
-  var restaurants = request.response;
+  const restaurants = request.response;
   listRestaurantsHTML(restaurants);
-  addNewMarker(restaurants);
-  restaurantID.addEventListener('click', function(e)
-    {
-      infoRestaurants(restaurants);          
-    });  
+  addNewMarker(restaurants);  
 }
 
 function calculateAverageRatingRestaurant(restaurant)
 {
- var totalRating = 0;
- var averageRating = 0;
+ let totalRating = 0;
+ let averageRating = 0;
 
  restaurant.ratings.forEach(function (ratings)
  {
@@ -34,26 +30,26 @@ function calculateAverageRatingRestaurant(restaurant)
  } else {
   averageRating = totalRating/restaurant.ratings.length;
  }
- var roundedAverageRating = Math.round(averageRating*10)/10;
+ let roundedAverageRating = Math.round(averageRating*10)/10;
   return roundedAverageRating;
 }
 
 function listRestaurantsHTML(jsonObj) 
 {
-  var restos = jsonObj['restaurants'];
+  let restos = jsonObj['restaurants'];
   
   restos.forEach((restaurant)=> {
-    var totalStars = 5;
-    var averRatingToShow = calculateAverageRatingRestaurant(restaurant);
-    var starPercentage = Math.round((averRatingToShow/totalStars)*100);
-    var myArticle = document.createElement('article');
-    var myH5 = document.createElement('h5');
-    var myPara2 = document.createElement('p');
-    var myPara1 = document.createElement('p');
+    let totalStars = 5;
+    let averRatingToShow = calculateAverageRatingRestaurant(restaurant);
+    let starPercentage = Math.round((averRatingToShow/totalStars)*100);
+    let myArticle = document.createElement('article');
+    let myH5 = document.createElement('h5');
+    let myPara2 = document.createElement('p');
+    let myPara1 = document.createElement('p');
 
-    var divStarsOuter = document.createElement('div');
+    let divStarsOuter = document.createElement('div');
     divStarsOuter.className = 'stars-outer';
-    var divStarsInner = document.createElement('div');
+    let divStarsInner = document.createElement('div');
     divStarsInner.className = 'stars-inner';
     divStarsInner.style.width = `${starPercentage}%`;
 
@@ -61,52 +57,106 @@ function listRestaurantsHTML(jsonObj)
     myPara2.textContent = averRatingToShow + '  ';
     myPara1.textContent = 'Adresse: ' + restaurant.address;
     
-
     myPara2.appendChild(divStarsOuter);
     divStarsOuter.appendChild(divStarsInner);
     myArticle.appendChild(myH5);
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara1);
     restaurantID.appendChild(myArticle);  
-    
-  })
 
+    myArticle.addEventListener('click', function(e)
+    {
+      infoRestaurants(restaurant);          
+    }); 
+  })
 }
 
-function infoRestaurants(jsonObj) 
+function infoRestaurants(restaurant) 
 {
-  var restos = jsonObj['restaurants'];
-  document.getElementById("myModalLabel").innerHTML = ""; 
-  document.getElementById("myModalBody").innerHTML = "";
-  
-  restos.forEach((restaurant)=> 
-  {
-    $("#myModal").modal('show');
-    document.getElementById("myModalLabel").textContent = restaurant.restaurantName;
-    console.log(restaurant.restaurantName);
-    var myList = document.createElement('ul');
+  let myModalLabel = document.getElementById('myModalLabel');
+  let myModalBody = document.getElementById('myModalBody');
 
-    var ratings = restaurant.ratings;
-    for (var j = 0; j < ratings.length; j++) 
+  myModalLabel.innerHTML = ""; 
+  document.getElementById("myModalImg").innerHTML = ""; 
+  myModalBody.innerHTML = "";
+  
+  let pos = {
+    lat: restaurant.lat,
+    lng: restaurant.long
+  };
+
+  let location = pos.lat+","+ pos.lng;        
+
+    let totalStars = 5;
+    let averRatingToShow = calculateAverageRatingRestaurant(restaurant);
+    let starPercentage = Math.round((averRatingToShow/totalStars)*100);
+
+    $("#myModal").modal('show');
+
+    let myH6 = document.createElement('h6');
+    let myPara = document.createElement('p');
+    let myPara2 = document.createElement('p');
+    let divStarsOuter = document.createElement('div');
+    divStarsOuter.className = 'stars-outer';
+    let divStarsInner = document.createElement('div');
+    divStarsInner.className = 'stars-inner';
+    divStarsInner.style.width = `${starPercentage}%`;
+
+
+    myH6.textContent = restaurant.restaurantName ;
+    myPara.textContent = 'Adresse : ' + restaurant.address;
+    myPara2.textContent = averRatingToShow + '  ';
+
+    myPara2.appendChild(divStarsOuter);
+    divStarsOuter.appendChild(divStarsInner);
+    myModalLabel.appendChild(myH6);
+    myModalLabel.appendChild(myPara2);
+    myModalLabel.appendChild(myPara);
+
+
+    let url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location="+location+"&key=AIzaSyCA5arXTDsp5lB6iANmio2i9EER5jo6msM";
+    ajaxGet(url,function() 
     {
-      var listItem = document.createElement('li');
-      listItem.textContent = ratings[j].stars + ratings[j].comment;
-      myList.appendChild(listItem);
-      console.log(myList);
-      document.getElementById('myModalBody').appendChild(myList);
-    }
+    document.getElementById("myModalImg").src = url;    
+    })
+
+  restaurant.ratings.forEach((ratings)=> 
+  {
+    let totalStar = 5;
+    let nbStars = ratings.stars;
+    let percentageStars = Math.round((nbStars/totalStar)*100);
+
+    let myList = document.createElement('ul');
+    let listItem = document.createElement('li');
+    let myPara1 = document.createElement('p');
+    let myPara2 = document.createElement('p');
+    let divStarsOuter2 = document.createElement('div');
+    divStarsOuter2.className = 'stars-outer';
+    let divStarsInner2 = document.createElement('div');
+    divStarsInner2.className = 'stars-inner';
+    divStarsInner2.style.width = `${percentageStars}%`;
+
+    myPara1.textContent = nbStars + '  ';
+    myPara2.innerHTML = ratings.comment + '<hr/>';
+
+    myPara1.appendChild(divStarsOuter2);
+    divStarsOuter2.appendChild(divStarsInner2);
+    listItem.appendChild(myPara1);
+    listItem.appendChild(myPara2);
+    myList.appendChild(listItem);
+    myModalBody.appendChild(myList);
   })
 }
 
 function addNewMarker(jsonObj) 
 {
-  var restos = jsonObj['restaurants'];
-  var restaurant,LatLng ;
+  let restos = jsonObj['restaurants'];
+  let restaurant,LatLng ;
   for (i in restos)
   {
     restaurant=restos[i];
     LatLng = new google.maps.LatLng(restaurant.lat,restaurant.long);
-      var marker= new google.maps.Marker
+      let marker= new google.maps.Marker
       ({
         position: LatLng,
         map: map,
